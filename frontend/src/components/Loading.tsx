b6d0e8f10a76630b334243c47af1d634a2f1f348
Yslan Ramos <yslanramos.r@gmail.com>
2022-12-30 17:24:51 -0300

    fix: install react-loading deps (#21)

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -3,9 +3,6 @@ import ReactLoading from "react-loading";
 export const Loading = () => {
   return (
     <div data-testid="app-loading" aria-live="assertive">
-      {/**
-       * eslint-disable-next-line @typescript-eslint/ban-ts-comment
-       * @ts-ignore */}
       <ReactLoading type="bubbles" color="#D07014" />
     </div>
   );
